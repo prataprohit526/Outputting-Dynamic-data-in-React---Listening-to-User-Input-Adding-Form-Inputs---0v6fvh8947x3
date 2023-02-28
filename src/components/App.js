6bc0faf5074970452ec9b34077c684f2ad0c04c8
prataprohit526 <prataprohit526@gmail.com>
2023-02-28 11:16:44 +0530

    add change

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,19 @@ import React,{useState,useEffect} from 'react'
 import '../styles/App.css';
 const App = () => {
 //code here
+const [value, setValue]=useState();
+const [data, setData]=useState("______");
+   function changeValue(e){
+    setValue(e.target.value);
+   }
+   function clickMe(){
+setData(value);
+   }
   return (
     <div id="main">
-      <input id='input' onChange={}></input>
-      <button id='button' onClick={}>Click</button>
-      <p id='text'> Hello my name is ____ and I study at Newton School</p>
+      <input id='input' onChange={changeValue}></input>
+      <button id='button' onClick={clickMe}>Click</button>
+      <p id='text'> Hello my name is {data} and I study at Newton School</p>
     </div>
   )
 }
